7bb325a4100ff1aca8f93ff5f680b55c0447b93f
SHANAWAZ KHAN <SHANAWAZ KHAN@192.168.0.3>
2018-10-18 19:38:22 +0530

    added hibernate jars and developed HidernateUtil class

--- Lab2/src/com/lab4/HibernateUtil.java
+++ Lab2/src/com/lab4/HibernateUtil.java
@@ -1,5 +1,63 @@
 package com.lab4;
 
-public class HibernateUtil {
+import java.io.ObjectStreamException;
 
+import org.apache.commons.collections.StaticBucketMap;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+
+
+
+final public class HibernateUtil {
+
+	static  SessionFactory sf= null;
+	
+	static {
+		
+		
+		AnnotationConfiguration cfg =new AnnotationConfiguration();
+		
+		cfg= (AnnotationConfiguration) cfg.configure();
+		
+		sf = cfg.buildSessionFactory();
+		
+		
+	}
+	
+	private HibernateUtil() {
+		
+		
+		
+		
+	}
+	
+	final public static SessionFactory getSessionFactory() {
+		
+		
+		
+		return sf;
+	}
+	
+	public Object writeReplace()throws ObjectStreamException{
+		
+		
+		
+		return new Exception();
+	}
+	public Object readResolve()throws ObjectStreamException{
+		
+		
+		
+		return  new Exception();
+	}
+	
+	public Object clone()throws CloneNotSupportedException{
+		
+		
+		return new CloneNotSupportedException();
+	}
+	
+	
+	
 }
